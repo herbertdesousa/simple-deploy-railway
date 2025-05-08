aa31ccbda87fdde7269f082666124156de831e72
herbertdesousa <sousaherbert138@gmail.com>
2025-05-08 08:47:39 -0300

    feat: add fibonacci endpoint with input validation

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { BadRequestException, Controller, Get } from '@nestjs/common';
+import { BadRequestException, Controller, Get, Param } from '@nestjs/common';
 import axios from 'axios';
 import { AppService } from './app.service';
 
@@ -18,5 +18,20 @@ export class AppController {
     if (!url) throw new BadRequestException('url not provided');
     console.log(`Sending to ${url}`);
     await axios.get(url);
+  }
+
+  @Get('/fibonacci/:number')
+  getFibonacci(@Param('number') number: string): number {
+    const n = parseInt(number, 10);
+    if (isNaN(n) || n < 0) {
+      throw new BadRequestException('Invalid number provided');
+    }
+
+    const fibonacci = (num: number): number => {
+      if (num <= 1) return num;
+      return fibonacci(num - 1) + fibonacci(num - 2);
+    };
+
+    return fibonacci(n);
   }
 }
